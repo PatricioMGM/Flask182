2561bcd4c673a744fa75f32d95dfe37cb5bd17dc
PatricioMGM <115204170+PatricioMGM@users.noreply.github.com>
2023-06-04 17:56:42 -0600

    corregir alguna cosilla

--- Practiva numero 4/base de datos bebidas-1.sql
+++ Practiva numero 4/base de datos bebidas-1.sql
@@ -1,15 +1,6 @@
 DROP DATABASE IF EXISTS bebidas;
 create database bebidas;
 use bebidas;
-CREATE TABLE Bebidas (
-  id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
-  Nombre VARCHAR(50),
-  Precio DECIMAL(10,2),
-  id_clasificacion int not null,
-  id_marca int not null,
-  FOREIGN KEY (id_clasificacion) REFERENCES Clasificaciones(id) ON DELETE CASCADE,
-  FOREIGN KEY (id_marca) REFERENCES Marca(id) ON DELETE CASCADE
-);
 
 create table Clasificaciones (
 id int not null primary key auto_increment,
@@ -21,8 +12,18 @@ id int not null primary key auto_increment,
 Marca varchar(50)
 );
 
-create user 'almacenbeb'@'localhost' identified by '';
-grant all privileges on bebidas.* to 'almacenbeb'@'localhost';
+CREATE TABLE Bebidas (
+  id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+  Nombre VARCHAR(50),
+  Precio DECIMAL(10,2),
+  id_clasificacion int not null,
+  id_marca int not null,
+  FOREIGN KEY (id_clasificacion) REFERENCES Clasificaciones(id) ON DELETE CASCADE,
+  FOREIGN KEY (id_marca) REFERENCES Marca(id) ON DELETE CASCADE
+);
+
+-- create user 'almacenbeb'@'localhost' identified by '';
+-- grant all privileges on bebidas.* to 'almacenbeb'@'localhost';
 
 INSERT INTO Marca (Marca) VALUES 
 ('Coca-Cola'),
