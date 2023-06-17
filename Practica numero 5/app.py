b7e0463fe3ad15234285c1c45c5e3c70d565bdac
PatricioMGM <115204170+PatricioMGM@users.noreply.github.com>
2023-06-16 18:49:09 -0600

    Practica6: Vistas, envio informacion vista a rutas

--- Practica numero 5/app.py
+++ Practica numero 5/app.py
@@ -1,13 +1,10 @@
 
 import mysql.connector
-from flask import Flask
+from flask import Flask, render_template, request
 
 
 #inicializion del servidor flask
 app = Flask(__name__)
-
-
-#Declaracion de rutas
 
 # Configurar la conexión a la base de datos
 app.config['MYSQL_HOST'] = 'localhost'
@@ -15,45 +12,30 @@ app.config['MYSQL_USER'] = 'root'
 app.config['MYSQL_PASSWORD'] = ''
 app.config['MYSQL_DB'] = 'dbflask'
 
-# Crear una instancia de conexión
-db = mysql.connector.connect(
-    host=app.config['MYSQL_HOST'],
-    user=app.config['MYSQL_USER'],
-    password=app.config['MYSQL_PASSWORD'],
-    database=app.config['MYSQL_DB']
-)
-
+#Declaracion de rutas
 # ruta Index http://localhost:5000
 # ruta se compone del nombre y la funcion
 @app.route('/')
 def index():
-    # Realizar la consulta a la tabla
-    cursor = db.cursor()
-    cursor.execute("SELECT id, nombre FROM nombres")
-    results = cursor.fetchall()
-
-    # Cerrar la conexión a la base de datos
-    cursor.close()
-    db.close()
-
-    # Construir una cadena de texto con los resultados de la consulta
-    response = "ID\tNombre\n"
-    for result in results:
-        response += f"{result[0]}\t{result[1]}\n"
-
-    # Devolver la cadena de texto como respuesta
-    return response
+    
+    # Devolver la vista como respuesta
+    return render_template('index.html')
 
 
-@app.route('/guardar')
+@app.route('/guardar', methods=['POST'])
 def guardar():
-    return "Se gurado el album en la BD"
+    if request.method == 'POST':
+        titulo = request.form['txtTitulo']
+        artista = request.form['txtArtista']
+        anio = request.form['txtAnio']
+        print(titulo, artista, anio)
+    
+    return "La info del Album llegó a su ruta, amigo ;)"
+
 
 @app.route('/eliminar')
 def eliminar():
     return "Se Elimino el album en la BD"
-
-
 
 # Ejecucion
 if __name__== '__main__':
