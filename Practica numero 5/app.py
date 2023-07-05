f95ee857170c1d6b8934bed4fa1d0fc17a8ec1d4
PatricioMGM <115204170+PatricioMGM@users.noreply.github.com>
2023-07-05 14:14:22 -0600

    Practica 8: habilitar la opcion de eliminar y editar

--- Practica numero 5/app.py
+++ Practica numero 5/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, flash, redirect
+from flask import Flask, render_template, request, flash, redirect, url_for
 from flask_mysqldb import MySQL
 
 app = Flask(__name__)
@@ -38,9 +38,44 @@ def guardar():
     flash("Se ha guardado el nuebo album")
     return redirect("/")
 
-@app.route('/eliminar')
-def eliminar():
-    return "Se eliminó el álbum en la BD"
+@app.route('/editar/<id>')
+def editar(id):
+    cursoreditar = mysql.connection.cursor()
+    cursoreditar.execute('select * from albums where id = %s', (id,))
+    consulta = cursoreditar.fetchone()
+    
+    return render_template('editarAlbum.html', consulta = consulta)
+
+@app.route('/actualizar/<id>', methods=['POST'])
+def actualizar(id):
+    if request.method == 'POST':
+        Vtitulo = request.form['txtTitulo']
+        Vartista = request.form['txtArtista']
+        Vanio = request.form['txtAnio']
+        
+        curActu = mysql.connection.cursor()
+        curActu.execute('update albums set titulo = %s, artista = %s, año = %s where id = %s', (Vtitulo, Vartista, Vanio, id))
+        mysql.connection.commit()
+        
+        flash("Album actualizado correctamente")
+        return redirect(url_for("index"))
+
+@app.route('/eliminaraviso/<id>')
+def eliminaraviso(id):
+    cursoreliminarav = mysql.connection.cursor()
+    cursoreliminarav.execute('select * from albums where id = %s', (id,))
+    consulta = cursoreliminarav.fetchone()
+
+    return render_template('eliminar.html', consulta = consulta)
+
+@app.route('/eliminar/<id>')
+def eliminar(id):
+    cursoreliminar = mysql.connection.cursor()
+    cursoreliminar.execute('delete from albums where id = %s', (id,))
+    mysql.connection.commit()
+    
+    flash("Album eliminado correctamente")
+    return redirect(url_for("index"))
 
 
 if __name__ == '__main__':
