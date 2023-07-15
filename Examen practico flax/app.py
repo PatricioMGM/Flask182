3b7791745fe5f4254ebadf7ee25283117da0d0fd
PatricioMGM <115204170+PatricioMGM@users.noreply.github.com>
2023-07-14 18:11:02 -0600

    avance del examen: muestra todos los datos de las flores, puedes buscar una en especifico y puedes abrir el archivo de etitar pero todavia no funciona

---
+++ Examen practico flax/app.py
@@ -0,0 +1,79 @@
+from flask import Flask, render_template, request, flash, redirect, url_for
+from flask_mysqldb import MySQL
+
+app = Flask(__name__, template_folder='templates', static_folder='public')
+
+# Configurar la conexión a la base de datos
+app.config['MYSQL_HOST'] = 'localhost'
+app.config['MYSQL_USER'] = 'root'
+app.config['MYSQL_PASSWORD'] = ''
+app.config['MYSQL_DB'] = 'DB_Floreria'
+app.secret_key = 'Mysecretkey'
+
+# Crear una instancia de la clase MySQL
+mysql = MySQL(app)
+
+@app.route('/')
+def index():
+    cgen = mysql.connection.cursor()
+    cgen.execute('select * from tbFlores')
+    flores = cgen.fetchall()
+    
+    return render_template('general.html', dflores = flores)
+
+@app.route('/iragregar')
+def agregar():
+    return render_template('ingresar.html')
+
+
+@app.route('/insertar', methods= ['POST'])
+def insertar():
+    if request.method == 'POST':
+        vflor = request.form['flor']
+        vcantidad = request.form['cantidad']
+        vprecio = request.form['precio']
+        
+        cagr = mysql.connection.cursor()
+        cagr.execute("Insert into tbFlores (Nombre, cantidad, precio) values (%s,%s,%s)", (vflor, vcantidad, vprecio))
+        mysql.connection.commit()
+
+    cgen = mysql.connection.cursor()
+    cgen.execute('select * from tbFlores')
+    flores = cgen.fetchall()
+    
+    return render_template('general.html', dflores = flores)
+
+@app.route('/irconsultaespecifica')
+def irconsultaespecifica():
+    return render_template('consultaespecifica.html')
+
+@app.route('/consultaespecifica', methods=['POST', 'GET'])
+def consultaespecifica():
+    if request.method == 'POST':
+        vflor = request.form['flor']
+        
+        curconesp = mysql.connect.cursor()
+        curconesp.execute('select * from tbFlores where nombre like(%s)', (vflor,))
+        datosflor = curconesp.fetchone()
+        
+        if datosflor:
+            return render_template('consultaEspecifica.html', flor = datosflor)
+        else:
+            flash('No se encontro la fruta')
+            return render_template('consultaEspecifica.html')
+    return render_template('consultaEspecifica.html')
+    
+@app.route('/ireditar/<id>')
+def ireditar(id):
+    
+    curconesp = mysql.connect.cursor()
+    curconesp.execute('select * from tbFlores where id = %s', (id,))
+    datosflor = curconesp.fetchone()
+    
+    return render_template('editarflor.html', flor = datosflor)
+
+@app.route
+
+
+if __name__ == '__main__':
+    app.run(port=5000, debug=True)
